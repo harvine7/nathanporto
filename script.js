740aaf572a31776dbcb9e1278ee021a73791a1b6
harvine7 <harvinkhong123@gmail.com>
2022-11-25 22:20:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function nathanFunction(){
+    document.documentElement.scrollTop = 3000;
+}
+
+function myskillsFunction(){
+    document.documentElement.scrollTop = 950;
+}
+
+function toolsFunction(){
+    document.documentElement.scrollTop = 2300;
+}
+
+function contactFunction(){
+    document.documentElement.scrollTop = 30000;
+}
